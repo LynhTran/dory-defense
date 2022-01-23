9d6111cb313819c562190c9f0f1c558684be9ff6
Lynh Tran <tran0879@umn.edu>
2022-01-22 20:42:35 -0600

    gmail api loading

---
+++ email.js
@@ -0,0 +1,45 @@
+//oauth2 auth
+chrome.identity.getAuthToken(
+	{'interactive': true},
+	function(){
+	  //load Google's javascript client libraries
+		window.gapi_onload = authorize;
+		loadScript('https://apis.google.com/js/client.js');
+	}
+);
+
+function loadScript(url){
+  var request = new XMLHttpRequest();
+
+	request.onreadystatechange = function(){
+		if(request.readyState !== 4) {
+			return;
+		}
+
+		if(request.status !== 200){
+			return;
+		}
+
+    eval(request.responseText);
+	};
+
+	request.open('GET', url);
+	request.send();
+}
+
+function authorize(){
+  gapi.auth.authorize(
+		{
+			client_id: '768162506749-n1v1g4kacsfcel9chrh7hjlkc1dlkb2i.apps.googleusercontent.com',
+			immediate: true,
+			scope: 'https://www.googleapis.com/auth/gmail.modify'
+		},
+		function(){
+		  gapi.client.load('gmail', 'v1', gmailAPILoaded);
+		}
+	);
+}
+
+
+	request.execute();
+}
