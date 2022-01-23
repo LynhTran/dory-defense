5fe8f1eaa46a8012af30aa6d995433332e26c418
Joshua Huang <Paralysing@users.noreply.github.com>
2022-01-23 10:31:07 -0600

    Fix toggles, clean up code

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
-let emailOn = true;
-let blockerOn = true;
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("initialized");
-  chrome.storage.sync.set({emailOn},
-  function() {
-  });
-  chrome.storage.sync.set({blockerOn},
-  function() {
-  })
+chrome.runtime.onInstalled.addListener(function(details){
+    if (details.reason == "install"){
+        chrome.storage.sync.set({'emailOn': true});
+        chrome.storage.sync.set({'blockerOn': true});
+    } else if (details.reason == "update"){
+        console.log('Updated')
+    }
 });
--- options.js
+++ options.js
@@ -1,19 +1,17 @@
 chrome.storage.sync.get(['emailOn'], function(result) {
     phishingToggler.checked = result.emailOn;
-  });
+});
+
 chrome.storage.sync.get(['blockerOn'], function(result) {
     blockerToggler.checked = result.blockerOn;
-  });
+});
 
+phishingToggler.addEventListener("change", function () {
+    console.log(this.checked);
+    chrome.storage.sync.set({'emailOn': this.checked});
+});
 
-
-phishingToggler.addEventListener("change", function ()  {
-    chrome.storage.sync.set({emailOn : this.checked});
-    chrome.storage.sync.get(['emailOn'], function() {
-    })});
-    
-
-blockerToggler.addEventListener("change", function ()  {
-    chrome.storage.sync.set({blockerOn : this.checked});
-    chrome.storage.sync.get(['blockerOn'], function() {
-      })}); 
+blockerToggler.addEventListener("change", function () {
+    console.log(this.checked);
+    chrome.storage.sync.set({'blockerOn': this.checked});
+});
